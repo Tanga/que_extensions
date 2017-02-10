1149c27410c14aa77c6b996ee961501e49f6ef07
Carlos Barros <carlos.barros@barros.ws>
2017-02-09 16:12:45 -0800

    removed uneeded stuff

--- lib/que_job_relationship.rb
+++
@@ -1,5 +0,0 @@
-class QueJobRelationship < ActiveRecord::Base
-  self.primary_key = :job_id
-  belongs_to :job, class_name: QueJob
-  belongs_to :parent, class_name: QueJob
-end
--- lib/que_job_status.rb
+++ lib/que_job_status.rb
@@ -1,5 +1,3 @@
 class QueJobStatus < ActiveRecord::Base
   self.table_name = :que_job_status
-
-  belongs_to :que_job
 end
